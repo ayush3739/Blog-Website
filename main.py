f05c070e577b5b42d97c61252e8c2fb09c9de2ad
Ayush Maurya <ayushmaurya21086@gmail.com>
2026-03-02 18:06:03 +0530

    change the post show in desc order by date

--- main.py
+++ main.py
@@ -215,7 +215,7 @@ def logout():
 
 @app.route('/')
 def get_all_posts():
-    result = db.session.execute(db.select(BlogPost))
+    result = db.session.execute(db.select(BlogPost).order_by(BlogPost.date.desc()))
     posts = result.scalars().all()
     return render_template("index.html", all_posts=posts,logged_in=current_user.is_authenticated)
 
