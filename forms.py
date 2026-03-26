2c725f0c997531b6e98e9677f0d87e7fdc635bfd
Ayush Maurya <ayushmaurya21086@gmail.com>
2026-03-26 13:14:06 +0530

    Refactor forms.py by removing TODO comments and updating footer and index templates to reflect the new establishment year

--- forms.py
+++ forms.py
@@ -4,7 +4,6 @@ from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
 
 
-# WTForm for creating a blog post
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
@@ -13,7 +12,6 @@ class CreatePostForm(FlaskForm):
     submit = SubmitField("Submit Post")
 
 
-# TODO: Create a RegisterForm to register new users
 class RegisterForm(FlaskForm):
     name=StringField("Name",validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
@@ -21,14 +19,12 @@ class RegisterForm(FlaskForm):
     submit=SubmitField("SIGN ME UP!")
     
 
-# TODO: Create a LoginForm to login existing users
 class LoginForm(FlaskForm):
     email=StringField("Email",validators=[DataRequired(),Email()])
     password=PasswordField("Password",validators=[DataRequired()])
     submit=SubmitField("LET ME IN!")
 
 
-# TODO: Create a CommentForm so users can leave comments below posts
 class CommentForm(FlaskForm):
     comment=CKEditorField("Comments", validators=[DataRequired()])
     submit=SubmitField('Submit Comments')
